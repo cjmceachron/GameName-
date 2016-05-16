8bbcb5d1ae090d7c8e0b11137ff9ee9fd2af7c89
Clayton McEachron <cjmceachron@yahoo.com>
2016-05-16 13:02:57 -0400

    Add files via upload

---
+++ source/types.py
@@ -0,0 +1,11 @@
+#project cheekymon
+#5/16/2016
+#Clayton McEachron
+#types.py
+
+class type(object):
+    def _init_(self, name, strength, weakness ):
+        self.name = name
+        self.strength = strength
+        self.weakness = weakness
+    
