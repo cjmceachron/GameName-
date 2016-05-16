c81d68ea91c72cdba48fa927c054eb5522fdcc44
Rob D'Andrea <dandrer@sunyit.edu>
2016-05-16 12:57:21 -0400

    Organising files

---
+++ source/trainer.py
@@ -0,0 +1,14 @@
+class Trainer:
+	'Base class for human and computer trainers'
+	
+	def __init__(self, name, cheekymons, saved_mons, location, stats, inventory) #texture ids?
+		self. name = name
+		self.cheekymons = cheekymons # list of Mon objects between 0-6 mons at a time
+		self.saved_mons = saved_mons # list of Mons not in cheekymons but owned
+		self.location = location # ["zone", x, y]
+		self.stats = stats
+		self.inventory = inventory
+		
+# Split into two children, human and ai
+# Human has inputs
+# ai has a path and engagement zone
